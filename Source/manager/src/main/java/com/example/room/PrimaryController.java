10e9fa0c167fbb1fcbac55de74d56a758c664ef0
Bottle1985 <mrboongvn@yahoo.com.vn>
2023-04-07 15:34:52 +0700

    - Tested button function

--- Source/manager/src/main/java/com/example/room/PrimaryController.java
+++ Source/manager/src/main/java/com/example/room/PrimaryController.java
@@ -9,4 +9,7 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    @FXML
+    private void testSecondary() throws IOException {
+    }
 }
