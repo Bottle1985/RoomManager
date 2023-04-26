9b2e29e65d5ebcc0e4c2f8e43473f8d2f551f759
Bottle1985 <mrboongvn@yahoo.com.vn>
2023-04-26 14:58:11 +0700

    + Added text for button login

--- Source/manager/src/main/java/com/example/room/PrimaryController.java
+++ Source/manager/src/main/java/com/example/room/PrimaryController.java
@@ -2,14 +2,16 @@ package com.example.room;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class PrimaryController {
-
+    @FXML private Text actiontarget;
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
     @FXML
-    private void testSecondary() throws IOException {
+    private void handleSubmitButtonAction() throws IOException {
+        actiontarget.setText("Sign in button pressed");
     }
 }
